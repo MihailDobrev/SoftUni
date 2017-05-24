6ed229f783e51b0b72fa4c3aa6c83f70b6f13c9a
Mihail Dobrev <mihail.m.dobrev@abv.bg>
2017-05-24 14:43:45 +0300

    Added all solutions to C% Intro and Basic Syntax-Exercises.

---
+++ Tech Module/Programming Fundamentals/1.C# Intro and Basic Syntax/3. Miles to Kilometers/MilestoKilometers.cs
@@ -0,0 +1,14 @@
+﻿
+namespace _3.Miles_to_Kilometers
+{
+    using System;
+    public class MilestoKilometers
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{number*1.60934:F2}");
+        }
+    }
+}
