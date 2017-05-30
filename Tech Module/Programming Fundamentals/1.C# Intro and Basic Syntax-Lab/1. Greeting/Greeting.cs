7139188fd57adb7008cf27cf8bda0b3bf54ab1f0
Mihail <mihail.m.dobrev@abv.bg>
2017-05-30 15:53:20 +0200

    Added 1.C# Intro and Basic Syntax-Lab.

---
+++ Tech Module/Programming Fundamentals/1.C# Intro and Basic Syntax-Lab/1. Greeting/Greeting.cs
@@ -0,0 +1,13 @@
+﻿
+namespace _1.Greeting
+{
+    using System;
+    public class Greeting
+    {
+        static void Main()
+        {
+            var name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
