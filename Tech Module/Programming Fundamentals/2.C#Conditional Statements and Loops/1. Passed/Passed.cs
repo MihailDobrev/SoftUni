c3096778f8027f0547a4fed7dd4e316296acc38a
Mihail <mihail.m.dobrev@abv.bg>
2017-05-30 15:44:28 +0200

    Added C#Conditional Statements and Loops.

---
+++ Tech Module/Programming Fundamentals/2.C#Conditional Statements and Loops/1. Passed/Passed.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Problem_1.Passed
+{
+    using System;
+    public class Passed
+    {
+        static void Main()
+        {
+            double grade = double.Parse(Console.ReadLine());
+
+            if (grade>=3.00)
+            {
+                Console.WriteLine("Passed!");
+            }
+            
+        }
+    }
+}
---
+++ Tech Module/Programming Fundamentals/2.C#Conditional Statements and Loops/7. Divisible by 3/Divisibleby3.cs
@@ -0,0 +1,18 @@
+﻿
+namespace _7.Divisible_by_3
+{
+    using System;
+    public class Divisibleby3
+    {
+        static void Main()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
