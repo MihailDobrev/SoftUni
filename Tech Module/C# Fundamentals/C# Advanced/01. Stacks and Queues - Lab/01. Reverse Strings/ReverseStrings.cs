3be91468e6285b7dcf5a403f2cd3d862afa02b2c
Mihail Dobrev <mihail.m.dobrev@abv.bg>
2017-10-25 13:54:58 +0300

    Added the first Lab from the first C# Advanced course - Stacks and Queues.

---
+++ Tech Module/C# Fundamentals/C# Advanced/01. Stacks and Queues - Lab/01. Reverse Strings/ReverseStrings.cs
@@ -0,0 +1,27 @@
+﻿namespace _01.Reverse_Strings
+{
+    using System;
+    using System.Collections.Generic;
+
+    class ReverseStrings
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+
+            Stack<char> stack = new Stack<char>();
+
+            foreach (var letter in input)
+            {
+                stack.Push(letter);
+            }
+            char output;
+
+            foreach (var letter in input)
+            {
+                output = stack.Pop();
+                Console.Write(output);
+            }
+        }
+    }
+}
