bfb65caf8d9db4c83907949f8ca9ce69a352a351
Mihail Dobrev <mihail.m.dobrev@abv.bg>
2017-10-25 14:12:45 +0300

    Added the first Lab of C% Fundamentals - Stacks and Queues.

---
+++ C# Fundamentals/C# Advanced/01. Stacks and Queues - Lab/02. Simple Calculator/SimpleCalculator.cs
@@ -0,0 +1,44 @@
+﻿namespace _02.Simple_Calculator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class SimpleCalculator
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine()
+                    .Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+        
+            Stack<int> addedSumStack = new Stack<int>();
+            Stack<int> substractedSumStack = new Stack<int>();
+
+            int finalSum = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string letter = input[i];
+                string nextLetter = input[i+1];
+
+                if (i == 0)
+                {
+                    addedSumStack.Push(int.Parse(letter));
+                }
+                else if (letter=="+")
+                {
+                    addedSumStack.Push(int.Parse(nextLetter));
+                    i++;
+                }
+                else if (letter == "-")
+                {
+                    substractedSumStack.Push(int.Parse(nextLetter));
+                    i++;
+                }
+            }
+
+            finalSum = addedSumStack.Sum() - substractedSumStack.Sum();
+            Console.WriteLine(finalSum);
+        }
+    }
+}
---
+++ C# Fundamentals/C# Advanced/01. Stacks and Queues - Lab/03. Decimal to Binary Converter/DecimaltoBinaryConverter.cs
@@ -0,0 +1,37 @@
+﻿namespace _03.Decimal_to_Binary_Converter
+{
+    using System;
+    using System.Collections.Generic;
+
+    class DecimaltoBinaryConverter
+    {
+        static void Main()
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            Stack<int> stack = new Stack<int>();
+
+            if (input==0)
+            {
+                Console.WriteLine(input);
+            }
+            else
+            {
+                while (input > 0)
+                {
+                    stack.Push(input % 2);
+                    input = input / 2;
+                }
+
+                int elementsCount = stack.Count;
+
+                for (int i = 0; i < elementsCount; i++)
+                {
+                    int removedNumber = stack.Pop();
+                    Console.Write(removedNumber);
+                }
+            }
+           
+        }
+    }
+}
