84a1d4b579ed11ecbbbcd5a8b4bc37a7ddca74ae
Mihail Dobrev <mihail.m.dobrev@abv.bg>
2018-07-05 14:33:36 +0300

    Uploading 02. ORM Fundamentals-MiniORM to Databases Advanced - Entity Framework

---
+++ C# DB/Databases Advanced - Entity Framework/02. ORM Fundamentals-MiniORM/MiniORM.App/StartUp.cs
@@ -0,0 +1,33 @@
+﻿namespace MiniORM.App
+{
+    using MiniORM.App.Data;
+    using MiniORM.App.Data.Entities;
+    using System.Linq;
+    public class StartUp
+    {
+        public static void Main()
+        {
+            const string connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=MiniORM;Integrated Security=True";
+
+            var context = new SoftUniDbContext(connectionString);
+
+            context.Employees.Add(new Employee
+            {
+                FirstName = "Gosho",
+                LastName = "Ivanov",
+                DepartmentId = context.Departments.First().Id,
+                IsEmployed = true,
+            });
+
+            var employee = context.Employees.Last();
+
+            employee.FirstName = "Pesho";
+
+            context.SaveChanges();
+
+            //Test
+            System.Console.WriteLine($"First Name:{employee.FirstName}, Last Name:{employee.LastName}");
+
+        }
+    }
+}
