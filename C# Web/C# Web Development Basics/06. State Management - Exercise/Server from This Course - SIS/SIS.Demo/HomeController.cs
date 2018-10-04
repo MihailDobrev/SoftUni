195b99cc5baac7d11d586b10f722a611639f156b
MihailDobrev <mihail.m.dobrev@abv.bg>
2018-10-04 06:11:36 +0300

    Uploading 06. State Management - Exercise for SIS Server.

---
+++ C# Web/C# Web Development Basics/06. State Management - Exercise/Server from This Course - SIS/SIS.Demo/HomeController.cs
@@ -0,0 +1,16 @@
+﻿namespace SIS.Demo
+{
+    using SIS.HTTP.Enums;
+    using SIS.HTTP.Response.Contracts;
+    using SIS.WebServer.Results;
+
+    public class HomeController
+    {
+        private const string randomContent = "<h1>Hello, World!</h1>";
+
+        public IHttpResponse Index()
+        {
+            return new HtmlResult(randomContent, HttpResponseStatusCode.OK);
+        }
+    }
+}
---
+++ C# Web/C# Web Development Basics/06. State Management - Exercise/Server from This Course - SIS/SIS.Demo/Launcher.cs
@@ -0,0 +1,20 @@
+﻿namespace SIS.Demo
+{
+    using SIS.HTTP.Enums;
+    using SIS.WebServer;
+    using SIS.WebServer.Routing;
+
+    public class Launcher
+    {
+        public static void Main()
+        {
+            ServerRoutingTable serverRoutingTable = new ServerRoutingTable();
+
+            serverRoutingTable.Routes[HttpRequestMethod.Get]["/"] = request => new HomeController().Index();
+
+            Server server = new Server(8000, serverRoutingTable);
+
+            server.Run();
+        }
+    }
+}
