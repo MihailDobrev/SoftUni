f1c0212636e3293a3e00bf223c4ce841c318eab7
Mihail Dobrev <mihail.m.dobrev@abv.bg>
2018-12-20 16:43:14 +0200

    Uploading 01. Linear-Data-Structures - Lab to Data Structures course.

---
+++ Open Courses/Data Structures/01. Linear-Data-Structures - Lab/Lists/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        ArrayList<int> list = new ArrayList<int>();
+        list.Add(5);
+        list[0] = list[0] + 1;
+        int element = list.RemoveAt(0);
+        System.Console.WriteLine(element);
+    }
+}
