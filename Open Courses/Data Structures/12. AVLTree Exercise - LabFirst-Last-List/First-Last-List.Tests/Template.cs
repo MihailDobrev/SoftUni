8e7702895c66e8842feded794cd9a5f31cb0a42f
Mihail <mihail.m.dobrev@abv.bg>
2019-02-06 22:55:01 +0200

    Uploading 12. AVLTree Exercise - LabFirst-Last-List to Data Structures course.

---
+++ Open Courses/Data Structures/12. AVLTree Exercise - LabFirst-Last-List/First-Last-List.Tests/Template.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+
+[TestFixture]
+public class TestClass1
+{
+    [Test]
+    public void TestMethod1()
+    {
+
+    }
+}
+
+class Product : IComparable<Product>
+{
+    public string Title { get; set; }
+    public decimal Price { get; set; }
+
+    public Product(decimal price, string title)
+    {
+        this.Price = price;
+        this.Title = title;
+    }
+
+    public int CompareTo(Product other)
+    {
+        return this.Price.CompareTo(other.Price);
+    }
+}
---
+++ Open Courses/Data Structures/12. AVLTree Exercise - LabFirst-Last-List/First-Last-List/FirstLastList.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Wintellect.PowerCollections;
+
+public class FirstLastList<T> : IFirstLastList<T> where T : IComparable<T>
+{
+    private LinkedList<T> byInsertion;
+    private OrderedBag<LinkedListNode<T>> byAscending;
+    private OrderedBag<LinkedListNode<T>> byDescending;
+    
+    public FirstLastList()
+    {
+        this.byInsertion = new LinkedList<T>();
+        this.byAscending = new OrderedBag<LinkedListNode<T>>((x, y) => x.Value.CompareTo(y.Value));
+        this.byDescending = new OrderedBag<LinkedListNode<T>>((x, y) => y.Value.CompareTo(x.Value));
+    }
+
+    public int Count
+    {
+        get => byInsertion.Count;
+    }
+
+    public void Add(T element)
+    {
+        LinkedListNode<T> node = new LinkedListNode<T>(element);
+        this.byInsertion.AddLast(node);
+        this.byAscending.Add(node);
+        this.byDescending.Add(node);
+    }
+
+    public void Clear()
+    {
+        this.byInsertion.Clear();
+        this.byAscending.Clear();
+        this.byDescending.Clear();
+    }
+
+    public IEnumerable<T> First(int count)
+    {
+        if (!CountIsInBounds(count))
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        LinkedListNode<T> current = this.byInsertion.First;
+
+        int start = 0;
+
+        while (start < count)
+        {
+            yield return current.Value;
+            current = current.Next;
+            start++;
+        }
+    }
+
+    private bool CountIsInBounds(int count)
+    {
+        return count >= 0 && count <= this.Count;
+    }
+
+    public IEnumerable<T> Last(int count)
+    {
+        if (!CountIsInBounds(count))
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        LinkedListNode<T> current = this.byInsertion.Last;
+
+        int start = 0;
+
+        while (start < count)
+        {
+            yield return current.Value;
+            current = current.Previous;
+            start++;
+        }
+    }
+
+    public IEnumerable<T> Max(int count)
+    {
+        if (!CountIsInBounds(count))
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        var maxElements = byDescending.Take(count);
+
+        foreach (var item in maxElements)
+        {
+            yield return item.Value;
+        }
+    }
+
+    public IEnumerable<T> Min(int count)
+    {
+        if (!CountIsInBounds(count))
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        var minElements = byAscending.Take(count);
+
+        foreach (var item in minElements)
+        {
+            yield return item.Value;
+        }
+    }
+
+    public int RemoveAll(T element)
+    {
+        LinkedListNode<T> node = new LinkedListNode<T>(element);
+
+        foreach (var item in this.byAscending.Range(node, true, node, true))
+        {
+            this.byInsertion.Remove(item);
+        }
+
+        int count = this.byAscending.RemoveAllCopies(node);
+        this.byDescending.RemoveAllCopies(node);
+
+        return count;
+    }
+}
