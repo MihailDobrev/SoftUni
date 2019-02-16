19fdd48c2c08f603087fdf20944b6090a2656423
Mihail <mihail.m.dobrev@abv.bg>
2019-02-16 18:05:15 +0200

    Uploading 16. Combining Data Structures - Lab to Data Structures course.

---
+++ Open Courses/Data Structures/16. Combining Data Structures - Lab/PersonCollection/PersonCollection.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections.Generic;
+using Wintellect.PowerCollections;
+
+public class PersonCollection : IPersonCollection
+{
+    private Dictionary<string, Person> personsByEmail = new Dictionary<string, Person>();
+    private Dictionary<string, SortedSet<Person>> personByEmailDomain = new Dictionary<string, SortedSet<Person>>();
+    private Dictionary<string, SortedSet<Person>> personByNameAndTown = new Dictionary<string, SortedSet<Person>>();
+    private OrderedDictionary<int, SortedSet<Person>> personsByAge = new OrderedDictionary<int, SortedSet<Person>>();
+    private Dictionary<string, OrderedDictionary< int, SortedSet<Person>>> personsByTownAndAge = new Dictionary<string, OrderedDictionary<int, SortedSet<Person>>>();
+
+    public bool AddPerson(string email, string name, int age, string town)
+    {
+        if (this.FindPerson(email) != null)
+        {
+            return false;
+        }
+        var person = new Person
+        {
+            Name = name,
+            Age = age,
+            Email = email,
+            Town = town
+        };
+        personsByEmail.Add(email, person);
+
+        string emailDomain = this.ExtractEmailDomain(email);
+        this.personByEmailDomain.AppendValueToKey(emailDomain, person);
+
+        string nameAndTown = this.CombineNameAndTown(name, town);
+        this.personByNameAndTown.AppendValueToKey(nameAndTown, person);
+
+        this.personsByAge.AppendValueToKey(age, person);
+
+        this.personsByTownAndAge.EnsureKeyExists(town);
+        this.personsByTownAndAge[town].AppendValueToKey(age, person);
+
+        return true;
+    }
+
+    private string ExtractEmailDomain(string email)
+    {
+       var domain=email.Split('@')[1];
+       return domain;
+    }
+
+    public int Count
+    {
+        get
+        {
+            return this.personsByEmail.Count;
+        }
+    }
+
+    public Person FindPerson(string email)
+    {
+        Person person;
+        var personExists = this.personsByEmail.TryGetValue(email, out person);
+        return person;
+    }
+
+    public bool DeletePerson(string email)
+    {
+        var person = this.FindPerson(email);
+
+        if (person==null)
+        {
+            return false;
+        }
+
+        var personDeleted = this.personsByEmail.Remove(email);
+
+        string emailDomain = this.ExtractEmailDomain(email);
+        this.personByEmailDomain[emailDomain].Remove(person);
+
+        string nameAndTown = this.CombineNameAndTown(person.Name, person.Town);
+        this.personByNameAndTown[nameAndTown].Remove(person);
+
+        this.personsByAge[person.Age].Remove(person);
+
+        this.personsByTownAndAge[person.Town][person.Age].Remove(person);
+
+        return true;
+    }
+
+    public IEnumerable<Person> FindPersons(string emailDomain)
+    {
+        return this.personByEmailDomain.GetValuesForKey(emailDomain);
+    }
+
+    public IEnumerable<Person> FindPersons(string name, string town)
+    {
+        var nameAndTown = this.CombineNameAndTown(name, town);
+        return this.personByNameAndTown.GetValuesForKey(nameAndTown);
+    }
+
+    private string CombineNameAndTown(string name, string town)
+    {
+        const string seperator = "|!|";
+        return name + seperator + town;
+    }
+
+    public IEnumerable<Person> FindPersons(int startAge, int endAge)
+    {
+        var personsInRange =
+            this.personsByAge.Range(startAge, true, endAge, true);
+
+        foreach (var personsByAge in personsInRange)
+        {
+            foreach (var person in personsByAge.Value)
+            {
+                yield return person;
+            }
+        }
+    }
+
+    public IEnumerable<Person> FindPersons(
+        int startAge, int endAge, string town)
+    {
+        if (!this.personsByTownAndAge.ContainsKey(town))
+        {
+            yield break;
+        }
+        var personsInRange =
+            this.personsByTownAndAge[town].Range(startAge, true, endAge, true);
+
+        foreach (var personsByTownAndAge in personsInRange)
+        {
+            foreach (var person in personsByTownAndAge.Value)
+            {
+                yield return person;
+            }
+        }
+    }
+}
---
+++ Open Courses/Data Structures/16. Combining Data Structures - Lab/PersonCollection/PersonCollectionSlow.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class PersonCollectionSlow : IPersonCollection
+{
+    private List<Person> persons = new List<Person>();
+
+    public bool AddPerson(string email, string name, int age, string town)
+    {
+        if (this.FindPerson(email) != null)
+        {
+            return false;
+        }
+        var person = new Person
+        {
+            Name = name,
+            Age = age,
+            Email = email,
+            Town = town
+        };
+        persons.Add(person);
+        return true;
+    }
+
+    public int Count
+    {
+        get
+        {
+            return this.persons.Count;
+        }
+    }
+
+    public Person FindPerson(string email)
+    {
+        return this.persons.FirstOrDefault(x => x.Email == email);
+    }
+
+    public bool DeletePerson(string email)
+    {
+        var person = this.FindPerson(email);
+
+        return this.persons.Remove(person);
+    }
+
+    public IEnumerable<Person> FindPersons(string emailDomain)
+    {
+        return this.persons
+             .Where(p => p.Email.EndsWith("@" + emailDomain))
+             .OrderBy(p => p.Email);
+    }
+
+    public IEnumerable<Person> FindPersons(string name, string town)
+    {
+        return this.persons
+              .Where(p => p.Name == name && p.Town == town)
+              .OrderBy(p => p.Email);
+    }
+
+    public IEnumerable<Person> FindPersons(int startAge, int endAge)
+    {
+        return this.persons
+              .Where(p => p.Age >= startAge && p.Age <= endAge)
+              .OrderBy(p => p.Age)
+              .ThenBy(p => p.Email);
+    }
+
+    public IEnumerable<Person> FindPersons(
+        int startAge, int endAge, string town)
+    {
+        return this.persons
+               .Where(p => p.Age >= startAge && p.Age <= endAge && p.Town == town)
+               .OrderBy(p => p.Age)
+               .ThenBy(p => p.Email);
+    }
+}
